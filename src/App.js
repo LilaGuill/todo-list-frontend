fcaded9ba177eeb5afee5196888b818781caca75
Lila Guillermic <lila.guillermic@gmail.com>
2020-02-08 08:58:53 +0100

    modification du composant Search en NewTask

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
-import Search from "./components/Search";
+import NewTask from "./components/NewTask";
 import Footer from "./components/Footer";
 import "./App.css";
 
---
+++ src/components/NewTask.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NewTask = () => {
+  return (
+    <div>
+      <form>
+        <input type="text"></input>
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
+};
+
+export default NewTask;
